2326092d109b6a0989ea939971ae5b3cd5a03944
EvilEntity <me@piotrjastrzebski.io>
2015-10-02 20:43:46 +0200

    initial commit

---
+++ desktop/src/io/piotrjastrzebski/btedit/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package io.piotrjastrzebski.btedit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.piotrjastrzebski.btedit.BTEdit;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.useHDPI = true;
+		new LwjglApplication(new BTEdit(), config);
+	}
+}
